425dcf709a7a44f639ca3ea4a4ed4b9c7fd50cdb
muradnuriyev001 <muradnuriyev001@gmail.com>
2024-04-10 19:18:09 +0400

    Add: Account Page

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -2,8 +2,38 @@ import usePageTitle from "../../hooks/usePageTitle";
 import c from "./Account.module.scss";
 
 const Account = () => {
-  usePageTitle("Booker | Account")
-  return <div>Account</div>;
+  usePageTitle("Booker | Account");
+  return (
+    <>
+      <div className={c.banner}>
+        <h1>Account</h1>
+      </div>
+      <div className={c.account}>
+        <div className={c.log_in}>
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder="Username or email adress*" />
+            <input type="password" placeholder="Password*" />
+            <div className={c.checkbox}>
+              <input type="checkbox" />
+              <p>Remember Me</p>
+            </div>
+          </form>
+          <button>LOGIN</button>
+        </div>
+
+        <div className={c.register}>
+          <h1>Register</h1>
+          <form action="">
+            <input type="text" placeholder="Email adress*" />
+            <input type="password" placeholder="Password*" />
+            <input type="password" placeholder="Repeat Password*" />
+          </form>
+          <button>REGISTER</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Account;
