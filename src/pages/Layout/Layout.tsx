396b91414192c61884ad795c80284884dadd6fb2
muradnuriyev001 <muradnuriyev001@gmail.com>
2024-04-11 12:48:23 +0400

    Add : Toast while adding to cart and wishlist

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -5,7 +5,11 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Loader from "../../components/Loader/Loader";
 import TopButton from "../../components/TopButton/TopButton";
-
+import { useSelector } from "react-redux";
+import { selectCart } from "../../redux/slices/cartSlice.slice";
+import { selectWishlist } from "../../redux/slices/wishlistSlice.slice";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Layout = () => {
   const { pathname } = useLocation();
 
@@ -19,10 +23,34 @@ const Layout = () => {
     window.scroll(0, 0);
   }, [pathname]);
 
+  const cartSelector = useSelector(selectCart);
+  const cartArray = Array.isArray(cartSelector.cart) ? cartSelector.cart : [];
+
+  const wishlistSelector = useSelector(selectWishlist);
+  const wishlistArray = Array.isArray(wishlistSelector.wishlist)
+    ? wishlistSelector.wishlist
+    : [];
+  useEffect(() => {
+    if (wishlistArray.length === 0) {
+      null;
+    } else {
+      toast.success("Successfullly added to wishlist");
+    }
+  }, [wishlistArray]);
+
+  useEffect(() => {
+    if (cartArray.length === 0) {
+      null;
+    } else {
+      toast.success("Successfullly added to cart");
+    }
+  }, [cartArray]);
+
   return (
     <>
       <div>
         {loaderState && <Loader />}
+        <ToastContainer limit={5} autoClose={1500} />
         <Header />
         <Outlet />
         <Footer />
